8fdf96018933784ef26b39ddbffb5994f0e68a20
이정은 <silver36c@gmail.com>
2024-03-12 10:34:19 +0900

    ✨ Feat: global exception handler 작성

--- backend/src/main/java/garden/carrot/toby/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/garden/carrot/toby/common/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package garden.carrot.toby.common.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import garden.carrot.toby.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,8 +16,17 @@ public class GlobalExceptionHandler {
 	private final StringBuilder sb = new StringBuilder();
 
 	@ExceptionHandler(Exception.class)
-	protected final ResponseEntity<String> handleAllExceptions(Exception ex) {
+	protected final ResponseEntity<ApiResponse<String>> handleAllExceptions(Exception ex) {
 		log.error("Exception 발생!", ex);
-		return ResponseEntity.badRequest().body(ex.getMessage());
+
+		ApiResponse<String> response = ApiResponse.globalError(HttpStatus.BAD_REQUEST, ex.getMessage());
+
+		// Custom Exception인 경우 원인 제공
+		if (ex instanceof CustomException customException) {
+			response = ApiResponse.error(customException.getErrorCode(),
+				(customException.getData().toString()));
+		}
+
+		return ResponseEntity.badRequest().body(response);
 	}
 }
