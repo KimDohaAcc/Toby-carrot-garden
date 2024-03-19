39be1847a29101d7c411855f2ce3a4589fca7b98
이정은 <silver36c@gmail.com>
2024-03-19 15:57:13 +0900

    ✨ Feat: 예제 컨트롤러

--- backend/src/main/java/garden/carrot/toby/hello/HelloController.java
+++ backend/src/main/java/garden/carrot/toby/hello/HelloController.java
@@ -1,20 +1,49 @@
 package garden.carrot.toby.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import garden.carrot.toby.common.constants.ErrorCode;
+import garden.carrot.toby.common.constants.SuccessCode;
 import garden.carrot.toby.common.dto.ApiResponse;
 import garden.carrot.toby.common.exception.CustomException;
+import garden.carrot.toby.hello.dto.HelloDto;
 
 @RestController
 public class HelloController {
 
 	@GetMapping("/hello")
-	public ApiResponse<String> hello() {
+	public ApiResponse<HelloDto.Response> getHello() {
 		String returnData = "Hello World!";
-		// throw CustomException.builder().errorCode(ErrorCode.BAD_PARAMETER).data(returnData).build();
-		throw new CustomException(ErrorCode.BAD_PARAMETER, returnData);
-		// return ApiResponse.success(SuccessCode.GET_SUCCESS, returnData);
+		HelloDto.Response dto = new HelloDto.Response(returnData);
+		return ApiResponse.success(SuccessCode.GET_SUCCESS, dto);
+	}
+
+	@GetMapping("/hello/query")
+	public ApiResponse<HelloDto.Response> getHello(@RequestParam String key) {
+		HelloDto.Response dto = new HelloDto.Response("쿼리 스트링: " + key);
+		return ApiResponse.success(SuccessCode.GET_SUCCESS, dto);
+	}
+
+	@GetMapping("/hello/path/{id}")
+	public ApiResponse<HelloDto.Response> getHelloPath(@PathVariable String id) {
+		HelloDto.Response dto = new HelloDto.Response("path variable: " + id);
+		return ApiResponse.success(SuccessCode.GET_SUCCESS, dto);
+	}
+
+	@PostMapping("/world")
+	public ApiResponse<HelloDto.Response> postHello(@RequestBody HelloDto.Request request) {
+		HelloDto.Response dto = new HelloDto.Response("당신이 보낸 것: " + request.getHello());
+		return ApiResponse.success(SuccessCode.POST_SUCCESS, dto);
+	}
+
+	@GetMapping("/hello/error")
+	public ApiResponse<HelloDto.Response> errorHello() {
+		String errorData = "에러 예제";
+		throw new CustomException(ErrorCode.BAD_PARAMETER, errorData);
 	}
 }
