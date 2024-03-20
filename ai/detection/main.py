585c3997cda3c34ef0a20a914d6a7bcef7b27f51
김지원 <wldnjs35242@naver.com>
2024-03-20 13:45:27 +0900

    ✨ Feat: detection 모델 변경
    Former-commit-id: 1ad48343db400aa20555f87b07ebf87c5e38d47d

--- ai/detection/main.py
+++ ai/detection/main.py
@@ -1,19 +1,29 @@
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+
+import tensorflow as tf
+from keras.models import load_model
+from tensorflow.keras.utils import img_to_array
+
+import io
+import PIL
+import cv2
+import random
+import base64
+import numpy as np
+
+from inceptionV3 import ref as inceptionV3
+
 from kafka import KafkaConsumer
 from json import loads
-import os
-import subprocess
 from s3_reader import s3_image_reader
-import tempfile
-import sys
 
-# 카프카 서버
 bootstrap_servers = ["host.docker.internal:9092"]
 
 # 카프카 토픽
 str_topic_name = 'detection'
 
 # 카프카 소비자 group1 생성
-str_group_name = 'group2'
+str_group_name = 'group1'
 consumer = KafkaConsumer(str_topic_name, bootstrap_servers=bootstrap_servers,
                          auto_offset_reset='earliest', # 가장 처음부터 소비
                          enable_auto_commit=True,
@@ -21,36 +31,37 @@ consumer = KafkaConsumer(str_topic_name, bootstrap_servers=bootstrap_servers,
                          value_deserializer=lambda x: loads(x.decode('utf-8')),
                          consumer_timeout_ms=60000 # 타임아웃지정(단위:밀리초)
                         )
+inceptionV3_model = tf.keras.applications.InceptionV3(
+        include_top=True,
+        weights="imagenet",
+        input_tensor=None,
+        input_shape=None,
+        pooling=None,
+        classes=1000,
+        classifier_activation="softmax",
+    )
 
 for message in consumer:
-    print("detection 모델 테스트")
-    print(message)
-    image_data, extension = s3_image_reader(message.value['imageUrl'])
+    print("그림 분석 요청", message)
+    image_data = s3_image_reader(message.value['imageUrl'])
+    # analyze_object(image_data, message.value['imageUrl'], message.value['memberId'], message.value['quizId'], message.value['correctAnswer'] )
 
-    # 이미지 데이터를 임시 파일로 저장
-    with tempfile.NamedTemporaryFile(delete=False, suffix=extension) as temp_file:
-        temp_file.write(image_data)
-        temp_file_path = temp_file.name
+    # 이미지 데이터를 PIL.Image 객체로 변환합니다.
+    image_load = PIL.Image.open(io.BytesIO(image_data))
+    # 이미지를 NumPy 배열로 변환합니다.
+    image = np.array(image_load)
 
-    # 필요한 경로 및 변수 설정
-    HOME = "content/"
-    weights_path = os.path.join(HOME, "weights", "gelan-c.pt")
-    device = "cpu"
+    # 이미지 천저리
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, [299, 299])
+    image = tf.expand_dims(image, 0)
 
-    # detect.py 스크립트를 실행하는 명령어 생성
-    correct_answer = message.value['correctAnswer']
-    quiz_id = message.value['quizId']
-    member_id = message.value['memberId']
+    # 모델 구동
+    prediction = inceptionV3_model.predict(image)[0]
 
-    python_executable = sys.executable
-
-    command = (f"{python_executable} detect.py --weights {weights_path} "
-               f"--source {temp_file_path} --conf 0.1 --device {device} "
-               f"--member_id {member_id} --quiz_id {quiz_id} --correct_answer {correct_answer}")
-    # subprocess.run()으로 명령어 실행
-    subprocess.run(command, shell=True)
-
-    # 임시 파일 삭제
-    os.unlink(temp_file_path)
-
-    print("Image analysis complete. Result saved.")
+    # 결과 후처리
+    idx = prediction.argmax()
+    result = inceptionV3[idx]
+    print(result)
+    # 반환
+    # answer_dto = ResultDto(image=image_b64, result=(result == imageDto.answer))
--- ai/detection/s3_reader.py
+++ ai/detection/s3_reader.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 dotenv_path = Path(".env")
 load_dotenv(dotenv_path=dotenv_path)
 
@@ -35,7 +34,7 @@ def s3_image_reader(s3_key):
     # S3에서 이미지 읽기
     response = s3.get_object(Bucket=AWS_BUCKET_NAME, Key=s3_key)
     image_data = response['Body'].read()
-    _, extension = os.path.splitext(s3_key)
+
     print("s3 객체 읽기 성공")
 
-    return image_data, extension
+    return image_data
