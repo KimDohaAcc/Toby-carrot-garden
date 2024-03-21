af6cd8ba5058ef24b6803426fb507a66c3afd3a7
이정은 <silver36c@gmail.com>
2024-03-21 15:24:53 +0900

    ✨ Feat: 예제 컨트롤러 경로 변경

--- backend/src/main/java/garden/carrot/toby/hello/HelloController.java
+++ backend/src/main/java/garden/carrot/toby/hello/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,22 +15,23 @@ import garden.carrot.toby.common.exception.CustomException;
 import garden.carrot.toby.hello.dto.HelloDto;
 
 @RestController
+@RequestMapping("hello")
 public class HelloController {
 
-	@GetMapping("/hello")
+	@GetMapping("/")
 	public ApiResponse<HelloDto.Response> getHello() {
 		String returnData = "Hello World!";
 		HelloDto.Response dto = new HelloDto.Response(returnData);
 		return ApiResponse.success(SuccessCode.GET_SUCCESS, dto);
 	}
 
-	@GetMapping("/hello/query")
+	@GetMapping("/query")
 	public ApiResponse<HelloDto.Response> getHello(@RequestParam String key) {
 		HelloDto.Response dto = new HelloDto.Response("쿼리 스트링: " + key);
 		return ApiResponse.success(SuccessCode.GET_SUCCESS, dto);
 	}
 
-	@GetMapping("/hello/path/{id}")
+	@GetMapping("/path/{id}")
 	public ApiResponse<HelloDto.Response> getHelloPath(@PathVariable String id) {
 		HelloDto.Response dto = new HelloDto.Response("path variable: " + id);
 		return ApiResponse.success(SuccessCode.GET_SUCCESS, dto);
@@ -41,7 +43,7 @@ public class HelloController {
 		return ApiResponse.success(SuccessCode.POST_SUCCESS, dto);
 	}
 
-	@GetMapping("/hello/error")
+	@GetMapping("/error")
 	public ApiResponse<HelloDto.Response> errorHello() {
 		String errorData = "에러 예제";
 		throw new CustomException(ErrorCode.BAD_PARAMETER, errorData);
