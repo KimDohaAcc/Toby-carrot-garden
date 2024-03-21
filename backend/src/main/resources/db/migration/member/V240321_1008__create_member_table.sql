2775d710a595548aa53c025b937f57c089544031
이정은 <silver36c@gmail.com>
2024-03-21 17:41:54 +0900

    💄 Style: 코드 포맷터 적용

--- backend/src/main/resources/db/migration/member/V240321_1008__create_member_table.sql
+++ backend/src/main/resources/db/migration/member/V240321_1008__create_member_table.sql
@@ -2,12 +2,12 @@ create table rabbit.member
 (
     id              int auto_increment
         primary key,
-    created_time    datetime(6)  null,
-    updated_time    datetime(6)  null,
-    birth_date      date         null,
-    nickname        varchar(30)  null,
+    created_time    datetime(6) null,
+    updated_time    datetime(6) null,
+    birth_date      date null,
+    nickname        varchar(30) null,
     parent_password varchar(255) null,
-    serial_number   bigint       null,
+    serial_number   bigint null,
     constraint UK_5mb6dtpv7rh76xaom4iavqkk9
         unique (serial_number)
 );
