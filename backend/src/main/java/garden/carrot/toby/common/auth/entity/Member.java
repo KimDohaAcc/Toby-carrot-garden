bceb5871272ecf6cb6da7cb5a0d82be0fac55cf3
이정은 <silver36c@gmail.com>
2024-03-21 10:34:47 +0900

    ✨ Feat: 멤버 엔티티 생성 및 SQL 추가

---
+++ backend/src/main/java/garden/carrot/toby/common/auth/entity/Member.java
@@ -0,0 +1,31 @@
+package garden.carrot.toby.common.auth.entity;
+
+import java.time.LocalDate;
+
+import garden.carrot.toby.common.entity.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+	@Setter
+	private String parentPassword;
+	@Setter
+	@Column(length = 30)
+	private String nickname;
+	@Setter
+	private LocalDate birthDate;
+	// 카카오에서 넘겨준 인증값
+	@Column(unique = true)
+	private long serialNumber;
+
+	public Member(long serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+}
