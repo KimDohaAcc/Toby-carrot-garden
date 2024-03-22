d88b0983d16bc193d0dcea7f910dbecdcc2ba271
이정은 <silver36c@gmail.com>
2024-03-22 15:14:18 +0900

    🐛 Fix: erd에 맞게 member table column 수정

--- backend/src/main/java/garden/carrot/toby/domain/member/entity/Member.java
+++ backend/src/main/java/garden/carrot/toby/domain/member/entity/Member.java
@@ -8,18 +8,14 @@ import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
-	@Setter
 	private String parentPassword;
-	@Setter
 	@Column(length = 30)
-	private String nickname;
-	@Setter
+	private String name;
 	private LocalDate birthDate;
 	// 카카오에서 넘겨준 인증값
 	@Column(unique = true)
