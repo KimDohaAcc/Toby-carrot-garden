dd4411bf78d9321e529af680e2666b68e719eaa5
전우리 <woori50300@gmail.com>
2024-03-25 13:46:10 +0900

    🔨Refact:Entity 수정

--- backend/src/main/java/garden/carrot/toby/domain/memberquiz/entity/MemberQuiz.java
+++ backend/src/main/java/garden/carrot/toby/domain/memberquiz/entity/MemberQuiz.java
@@ -1,6 +1,7 @@
 package garden.carrot.toby.domain.memberquiz.entity;
 
 import garden.carrot.toby.common.entity.BaseEntity;
+import garden.carrot.toby.domain.member.entity.Member;
 import garden.carrot.toby.domain.quizdata.entity.QuizData;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,14 +10,16 @@ import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberQuiz extends BaseEntity {
-//	@ManyToOne(fetch = FetchType.LAZY)
-//	@JoinColumn(name = "member_id")
-//	private Member member;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "quiz_data_id")
