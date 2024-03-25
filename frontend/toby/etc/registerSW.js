e896f38dff6227502af2e41f4417449c86d4c717
김도하 <dorong0205@gmail.com>
2024-03-25 22:50:25 +0900

    Fix: 주석 정리 및 경로 수정

--- frontend/toby/etc/registerSW.js
+++
@@ -1,9 +0,0 @@
-  export function registerSW() {
-    navigator.serviceWorker.register('/sw.js', {
-        // scope: '/' // /api 경로 제외
-      }).then(function(registration) {
-        console.log('Service Worker 등록 성공:', registration.scope);
-      }).catch(function(error) {
-        console.log('Service Worker 등록 실패:', error);
-      });
-  }
