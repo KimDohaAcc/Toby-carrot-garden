2c41a96a06cd6de81de1df4af38bb513da580723
이정은 <silver36c@gmail.com>
2024-03-27 14:50:59 +0900

    ✨ Feat: 이미지 업로드 2MB 용량 제한
    S3 꽉차지 않도록 용량제한 했어요

--- backend/src/main/java/garden/carrot/toby/common/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/garden/carrot/toby/common/exception/GlobalExceptionHandler.java
@@ -1,12 +1,15 @@
 package garden.carrot.toby.common.exception;
 
-import garden.carrot.toby.common.dto.ApiResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+
+import garden.carrot.toby.common.constants.ErrorCode;
+import garden.carrot.toby.common.dto.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @ControllerAdvice
@@ -16,22 +19,31 @@ public class GlobalExceptionHandler {
 	private final StringBuilder sb = new StringBuilder();
 	private final ExceptionUtil exceptionUtil;
 
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<ApiResponse<String>> handleCustomException(CustomException customException) {
+		log.error("Custom Exception 발생!", customException);
+
+		String stringData =
+			customException.getData() == null ? null : customException.getData().toString();
+		ApiResponse<String> response = ApiResponse.error(customException.getErrorCode(),
+			stringData);
+		return ResponseEntity.badRequest().body(response);
+	}
+
+	@ExceptionHandler(MaxUploadSizeExceededException.class)
+	public ResponseEntity<ApiResponse<String>> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex) {
+		log.error("handleMaxUploadSizeExceededException 발생!", ex);
+		ApiResponse<String> response = ApiResponse.error(ErrorCode.FILE_SIZE_LIMIT, null);
+		return ResponseEntity.badRequest().body(response);
+	}
+
 	@ExceptionHandler(Exception.class)
 	protected final ResponseEntity<ApiResponse<String>> handleAllExceptions(Exception ex) {
 		log.error("Exception 발생!", ex);
+		exceptionUtil.sendExceptionToDiscord(ex);
 
 		ApiResponse<String> response = ApiResponse.globalError(HttpStatus.INTERNAL_SERVER_ERROR,
 			ex.getMessage());
-
-		// Custom Exception인 경우 원인 제공
-		if (ex instanceof CustomException customException) {
-			String stringData =
-				customException.getData() == null ? null : customException.getData().toString();
-			response = ApiResponse.error(customException.getErrorCode(),
-				stringData);
-		} else {
-			exceptionUtil.sendExceptionToDiscord(ex);
-		}
 
 		return ResponseEntity.badRequest().body(response);
 	}
