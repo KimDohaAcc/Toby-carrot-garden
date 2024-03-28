1db722610da13e10e5f285261ee3a664881baad5
김지원 <wldnjs35242@naver.com>
2024-03-28 13:50:32 +0900

    🐛 Fix: 카프카 설정 변경

--- backend/src/main/java/garden/carrot/toby/api/story/util/KafkaProducer.java
+++ backend/src/main/java/garden/carrot/toby/api/story/util/KafkaProducer.java
@@ -1,26 +1,22 @@
 package garden.carrot.toby.api.story.util;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import garden.carrot.toby.api.story.dto.QuizDto.ProducerData;
-import garden.carrot.toby.common.constants.ErrorCode;
-import garden.carrot.toby.common.exception.CustomException;
 import garden.carrot.toby.common.exception.ExceptionUtil;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class KafkaProducer {
 
-	private KafkaTemplate<String, String> kafkaTemplate;
-	private ExceptionUtil exceptionUtil;
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	private final ExceptionUtil exceptionUtil;
 
 	public void sendMessage(ProducerData data, String topicName) throws JsonProcessingException {
 		try {
