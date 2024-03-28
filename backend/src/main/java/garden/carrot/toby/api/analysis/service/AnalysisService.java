902256c37250ca8236f94268e02e4bc9d42f597f
이정은 <silver36c@gmail.com>
2024-03-28 12:46:38 +0900

    🐛 Fix 리스트가 비어 있을 때 빈 리스트 보내기

--- backend/src/main/java/garden/carrot/toby/api/analysis/service/AnalysisService.java
+++ backend/src/main/java/garden/carrot/toby/api/analysis/service/AnalysisService.java
@@ -1,5 +1,11 @@
 package garden.carrot.toby.api.analysis.service;
 
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import garden.carrot.toby.api.auth.util.MemberUtil;
 import garden.carrot.toby.common.constants.ErrorCode;
 import garden.carrot.toby.common.exception.CustomException;
@@ -10,11 +16,6 @@ import garden.carrot.toby.domain.memberquiz.repository.MemberQuizRepository;
 import garden.carrot.toby.domain.quizdata.entity.QuizType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -22,40 +23,36 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class AnalysisService {
 
-    private final MemberQuizRepository memberQuizRepository;
-    private final MemberRepository memberRepository;
-    private final MemberUtil memberUtil;
-    private final PasswordEncoder passwordEncoder;
+	private final MemberQuizRepository memberQuizRepository;
+	private final MemberRepository memberRepository;
+	private final MemberUtil memberUtil;
+	private final PasswordEncoder passwordEncoder;
 
-    public List<MemberQuiz> getMemberQuizList(QuizType quizType) throws CustomException {
-        Member member = memberUtil.getLoginMember();
-        List<MemberQuiz> memberQuizList = memberQuizRepository.getMemberQuizByTypeAndMemberId(member.getId(), quizType);
-        if (memberQuizList.isEmpty()) {
-            throw new CustomException(ErrorCode.BAD_PARAMETER);
-        }
+	public List<MemberQuiz> getMemberQuizList(QuizType quizType) throws CustomException {
+		Member member = memberUtil.getLoginMember();
+		List<MemberQuiz> memberQuizList = memberQuizRepository.getMemberQuizByTypeAndMemberId(member.getId(), quizType);
+		return memberQuizList;
+	}
 
-        return memberQuizList;
-    }
+	@Transactional
+	public MemberQuiz updateScoreByMemberQuizId(int memberQuizId, double score) throws CustomException {
+		MemberQuiz memberQuiz = memberQuizRepository.findById(memberQuizId)
+			.orElseThrow(() -> new CustomException(ErrorCode.BAD_PARAMETER));
 
-    @Transactional
-    public MemberQuiz updateScoreByMemberQuizId(int memberQuizId, double score) throws CustomException {
-        MemberQuiz memberQuiz = memberQuizRepository.findById(memberQuizId)
-                .orElseThrow(() -> new CustomException(ErrorCode.BAD_PARAMETER));
+		memberQuiz.setScore(score);
+		memberQuizRepository.save(memberQuiz);
 
-        memberQuiz.setScore(score);
-        memberQuizRepository.save(memberQuiz);
+		return memberQuiz;
+	}
 
-        return memberQuiz;
-    }
+	public boolean verifyParentPassword(String parentPassword) throws CustomException {
+		Member memberLoginId = memberUtil.getLoginMember();
+		Member member = memberRepository.findById(memberLoginId.getId())
+			.orElseThrow(() -> new CustomException(ErrorCode.BAD_PARAMETER));
 
-    public boolean verifyParentPassword(String parentPassword) throws CustomException {
-        Member memberLoginId = memberUtil.getLoginMember();
-        Member member = memberRepository.findById(memberLoginId.getId())
-                .orElseThrow(() -> new CustomException(ErrorCode.BAD_PARAMETER));
-
-        if(member != null && passwordEncoder.matches(parentPassword, member.getParentPassword())){
-            return true;
-        }
-        return false;
-    }
+		if (member != null && passwordEncoder.matches(parentPassword, member.getParentPassword())) {
+			return true;
+		}
+		return false;
+	}
 }
