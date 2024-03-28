4a64262c3ac4b21e3f30153407528cbcbacc4204
김지원 <wldnjs35242@naver.com>
2024-03-28 16:59:31 +0900

    Update detection.py
    Former-commit-id: 2664050c42989d0ea30f1bc3a12e7d30aadc9873

--- ai/detection/detection.py
+++ ai/detection/detection.py
@@ -17,9 +17,6 @@ load_dotenv(dotenv_path=dotenv_path)
 REDIS_HOST = os.getenv("REDIS_HOST")
 REDIS_PORT = os.getenv("REDIS_PORT")
 
-cup = [3, 60, 117, 122, 248, 250, 347, 438, 440, 463, 470, 497, 503, 504, 512, 518, 572, 575, 626, 647, 659, 688, 719, 725, 727, 738, 766, 778, 792, 809, 813, 835, 849, 859, 876, 898, 899, 901, 968]
-bag = [406, 414, 456, 463, 464, 520, 529, 533, 553, 569, 588, 591, 636, 665, 692, 701, 728, 747, 748, 785, 790, 797, 840, 893]
-
 def detection(image_data, data_name, member_id, quiz_id, correct_answer, inceptionV3_model):
     try:
         # 이미지 데이터를 PIL.Image 객체로 변환합니다.
@@ -40,29 +37,14 @@ def detection(image_data, data_name, member_id, quiz_id, correct_answer, incepti
         idx = prediction.argmax()
         # result = inceptionV3[idx]
 
-        print("분석 결과  : " + inceptionV3[idx], flush=True)
-
-        print("prediction[483] : ", prediction[483])
-
-        print("prediction[414] _ bag : ", prediction[414])
-
-        print("prediction[438] : ", prediction[438])
-        print("prediction[441] : ", prediction[441])
-        print("prediction[647] : ", prediction[647])
-        print("prediction[898] : ", prediction[898])
-        print("prediction[968] : ", prediction[968])
-
-
         result = 0
-
-        if correct_answer == "cup" and prediction[438] < 4:
+        if correct_answer == "cup" and prediction[441] < 4:
             result = "100"
         if correct_answer == "bag" and prediction[414] < 4:
             result = "100"
 
-
-
         print(result, flush=True)
+
     except Exception as e:
         print("모델 에러 발생 ", e, flush=True)
 
