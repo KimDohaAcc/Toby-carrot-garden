d9cdbf5fb52e1a20f95ffd37af182921bdaf8363
이정은 <silver36c@gmail.com>
2024-03-28 13:51:23 +0900

    🔧 Chore: 더미 스토리 sql 추가

---
+++ backend/src/main/resources/db/migration/data/V240328_1302__insert_school_story2_data.sql
@@ -0,0 +1,7 @@
+insert into rabbit.story_data (id, created_time, updated_time, recommend_age, story_image_url, title, place_data_id)
+values (3, '2024-03-27 18:26:12.000000', '2024-03-27 18:26:14.000000', '3~4세',
+        'https://tobycarrotbucket.s3.ap-northeast-2.amazonaws.com/assets/image/school/story02/000.png', '토비의 학교생활',
+        1),
+       (4, '2024-03-27 18:26:14.000000', '2024-03-27 18:26:15.000000', '3~4세',
+        'https://tobycarrotbucket.s3.ap-northeast-2.amazonaws.com/assets/image/hospital/story02/000.png', '병문안을 간 토비',
+        2);
