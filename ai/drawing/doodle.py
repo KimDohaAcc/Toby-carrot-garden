10cf4496b2e1fbfc67d0df711f4259f11667ad5e
김지원 <wldnjs35242@naver.com>
2024-03-28 17:33:21 +0900

    Fix: 백분율 환산한 값 로그 확인
    Former-commit-id: 9b9d6690d4c48832776ec1b3d625958de578ecd4

--- ai/drawing/doodle.py
+++ ai/drawing/doodle.py
@@ -49,8 +49,12 @@ def analyze_object(image_data, object_name, member_id, quiz_id, correct_answer):
         prediction = doodle_model.predict(image_tensor)
 
         print("일치율 확인", flush=True)
-        print(prediction, flush=True)
 
+        probabilities = np.exp(prediction) / np.sum(np.exp(prediction))  # 소프트맥스 함수를 사용하여 로짓 값을 확률로 변환
+
+        # 백분율로 환산
+        percentages = probabilities * 100
+        print(percentages, flush=True)
 
         # 결과
         result = prediction[0][target_index] + 100
