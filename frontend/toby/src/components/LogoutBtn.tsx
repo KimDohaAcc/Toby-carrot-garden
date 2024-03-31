6746c0bf72892ec589273a9433337de35befbd0d
김지원 <wldnjs35242@naver.com>
2024-04-01 03:12:09 +0900

    :art: Feat: 마이페이지 로그아웃 기능

--- frontend/toby/src/components/LogoutBtn.tsx
+++ frontend/toby/src/components/LogoutBtn.tsx
@@ -1,16 +1,35 @@
 import React from "react";
 import styled from "styled-components";
+import { clearUserStorage } from "../apis/userStorageApi";
+import { useNavigate } from "react-router-dom";
+
 
 const LogoutArea = styled.div`
   display: flex;
   flex-direction: column;
   border: 2px solid black;
+  width: calc(8%);
+  margin: calc(1%);
+  font-size: calc(2rem);
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: calc(50%);
 `;
 
 const LogoutBtn = () => {
+
+  const navigate = useNavigate();
+  
+  const handleLogout = () => {
+    clearUserStorage();
+    navigate("/");
+  };
   return (
-    <LogoutArea onClick={() => alert("Logout 클릭임")}>
-      <img src="/logoutImage.png" alt="logout" />
+    <LogoutArea onClick={() => handleLogout()}>
+      <Image src="/Image/button/logoutButton.png" alt="logout" />
       <div>로그아웃</div>
     </LogoutArea>
   );
