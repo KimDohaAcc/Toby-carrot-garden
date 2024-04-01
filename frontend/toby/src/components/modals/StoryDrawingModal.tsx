ce59968406c8cc7fcf118adebe99afd816e455c9
오화석 <hwasuk528@naver.com>
2024-04-01 23:27:53 +0900

    라우터, 폴링 재수정

--- frontend/toby/src/components/modals/StoryDrawingModal.tsx
+++ frontend/toby/src/components/modals/StoryDrawingModal.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import styled from "styled-components";
 import SignatureCanvas from "react-signature-canvas";
 import { submitQuiz, getQuizAnswer } from "../../apis/quizApi";
 import WaitToby from "./WaitToby";
 import FailToby from "./FailToby";
 import SuccessToby from "./SuccessToby";
+
 const StoryDrawingModalContainer = styled.div`
   display: flex;
   position: absolute;
@@ -29,147 +30,120 @@ const CloseBtn = styled.button`
   position: absolute;
   bottom: 5px;
   right: 5px;
-  /* background-image: url("경로/이미지.png"); */
   background-size: cover;
   border: none;
 `;
 
 const StoryDrawingModal = ({ isOpen, onClose, quizId }) => {
   const signaturePadRef = useRef(null);
-  const modalRef = useRef(null);
   const [canvasSize, setCanvasSize] = useState({ width: 0, height: 0 });
-  const [modalState, setModalState] = useState<
-    "none" | "wait" | "success" | "fail"
-  >("none");
+  const [modalState, setModalState] = useState("none");
 
   useEffect(() => {
-    function updateCanvasSize() {
-      if (modalRef.current) {
-        const { width, height } = modalRef.current.getBoundingClientRect();
-        // Border의 두께를 고려하지 않는 경우, 아래와 같이 설정할 수 있습니다.
-        // 즉, ModalArea 자체의 크기를 직접 사용합니다.
-        setCanvasSize({ width, height });
-      }
+    if (isOpen) {
+      const updateCanvasSize = () => {
+        if (signaturePadRef.current) {
+          const { offsetWidth, offsetHeight } =
+            signaturePadRef.current.canvas.parentElement;
+          setCanvasSize({ width: offsetWidth, height: offsetHeight });
+        }
+      };
+
+      window.addEventListener("resize", updateCanvasSize);
+      updateCanvasSize();
+      return () => window.removeEventListener("resize", updateCanvasSize);
     }
-
-    updateCanvasSize();
-    window.addEventListener("resize", updateCanvasSize);
-
-    return () => window.removeEventListener("resize", updateCanvasSize);
   }, [isOpen]);
 
-  useEffect(() => {
-    if (modalState === "success" || modalState === "fail") {
-      const timer = setTimeout(() => {
-        setModalState("none");
-        onClose(); // Automatically close modal after 2 seconds
-      }, 2000);
-      return () => clearTimeout(timer);
+  const handleSaveDrawing = useCallback(async () => {
+    if (!signaturePadRef.current || !isOpen) return;
+
+    const dataUrl = signaturePadRef.current
+      .getTrimmedCanvas()
+      .toDataURL("image/png");
+    const response = await fetch(dataUrl);
+    const blob = await response.blob();
+    const file = new File([blob], "drawing.png", { type: "image/png" });
+
+    const formData = new FormData();
+    formData.append("analysisImage", file);
+    formData.append("quizId", quizId);
+
+    try {
+      const response = await submitQuiz(formData);
+      if (
+        response.status === 200 &&
+        response.data.result &&
+        response.data.result.memberQuizId
+      ) {
+        const memberQuizId = response.data.result.memberQuizId;
+        setModalState("wait");
+        checkQuizAnswer(memberQuizId);
+      } else {
+        setModalState("fail");
+      }
+    } catch (error) {
+      console.error("Quiz submission error", error);
+      setModalState("fail");
     }
-  }, [modalState, onClose]);
-  const handleSaveDrawing = async () => {
-    if (signaturePadRef.current && isOpen) {
-      const canvas = signaturePadRef.current.getCanvas();
-      const dataUrl = canvas.toDataURL("image/png");
+  }, [isOpen, quizId]);
 
-      // dataURL을 Blob으로 변환
-      const response = await fetch(dataUrl);
-      const blob = await response.blob();
+  const checkQuizAnswer = useCallback(async (memberQuizId) => {
+    let attempts = 0;
+    const maxAttempts = 10;
 
-      // Blob을 File 객체로 변환
-      const file = new File([blob], "drawing.png", { type: "image/png" });
-
-      const formData = new FormData();
-      formData.append("analysisImage", file);
-      formData.append("quizId", quizId.toString());
-
+    const interval = setInterval(async () => {
       try {
-        // submitQuiz 함수 호출 시 반환된 데이터를 response 변수에 할당
-        const submitResponse = await submitQuiz(formData);
-        const { status, result } = submitResponse;
+        const answerResponse = await getQuizAnswer({
+          member_quiz_id: memberQuizId,
+        });
 
-        if (status === 200 && response.data.result.memberQuizId) {
-          console.log("Quiz submitted successfully"); // 제출에 성공한 경우 memberQuizId 추출
-          const memberQuizId = submitResponse.data.result.memberQuizId;
-          console.log(memberQuizId);
-
-          console.log(response.data.result.memberQuizId);
-
-          setModalState("wait"); // 폴링 동안 WaitToby 모달 표시
-
-          const endTime = Date.now() + 10000; // 10초 후 폴링 종료
-          const intervalId = setInterval(async () => {
-            if (Date.now() > endTime) {
-              clearInterval(intervalId);
-              setModalState("fail"); // 10초 동안 분석 결과를 받지 못하면 FailToby 모달 표시
-              return;
-            }
-
-            // getQuizAnswer 호출 시 submitQuiz에서 받은 memberQuizId를 사용
-            const answerResponse = await getQuizAnswer({
-              member_quiz_id: memberQuizId,
-            });
-            console.log(memberQuizId);
-            if (
-              answerResponse.status === 200 &&
-              answerResponse.result.score !== -1
-            ) {
-              clearInterval(intervalId);
-              setModalState(
-                answerResponse.result.score === 100 ? "success" : "fail"
-              ); // 점수에 따라 SuccessToby 또는 FailToby 모달 표시
-            }
-          }, 1000);
+        if (
+          answerResponse.status === 200 &&
+          answerResponse.data.result.score !== -1
+        ) {
+          clearInterval(interval);
+          setModalState(
+            answerResponse.data.result.score === 100 ? "success" : "fail"
+          );
         } else {
-          console.error("Quiz submission failed", submitResponse.message);
-          setModalState("fail"); // 제출 실패 시 FailToby 모달 표시
+          console.error("Failed to get quiz answer");
         }
       } catch (error) {
-        console.error("이미지 전송 실패", error);
-        setModalState("fail"); // 전송 실패 시 FailToby 모달 표시
+        console.error("Error fetching quiz answer", error);
       }
-    }
-  };
 
-  // 모달 상태가 변경될 때마다 실행되는 useEffect
-  useEffect(() => {
-    let timer;
-    if (["success", "fail"].includes(modalState)) {
-      timer = setTimeout(() => {
-        setModalState("none"); // SuccessToby 또는 FailToby 모달을 2초 후 자동으로 닫음
-        onClose(); // 모달 닫기 콜백 함수 호출
-      }, 2000);
-    }
-    return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머 제거
-  }, [modalState, onClose]);
+      attempts++;
+      if (attempts >= maxAttempts) {
+        clearInterval(interval);
+        console.error("Max polling attempts reached, stopping.");
+        setModalState("fail");
+      }
+    }, 1000);
+  }, []);
 
   if (!isOpen) return null;
 
   return (
-    <StoryDrawingModalContainer>
-      <ModalArea ref={modalRef}>
-        <SignatureCanvas
-          ref={signaturePadRef}
-          penColor="black"
-          canvasProps={{
-            width: canvasSize.width,
-            height: canvasSize.height,
-            className: "signature-canvas",
-            style: { backgroundColor: "white" }, // 배경을 흰색으로 설정
-          }}
-          minWidth={5} // 펜 굵기 최소값
-          maxWidth={5} // 펜 굵기 최대값
-        />
-      </ModalArea>
-      <CloseBtn onClick={handleSaveDrawing}>다 그렸어요</CloseBtn>
+    <>
+      <StoryDrawingModalContainer>
+        <ModalArea>
+          <SignatureCanvas
+            ref={signaturePadRef}
+            penColor="black"
+            canvasProps={{
+              width: canvasSize.width,
+              height: canvasSize.height,
+              className: "signatureCanvas",
+            }}
+          />
+        </ModalArea>
+        <CloseBtn onClick={handleSaveDrawing}>Submit</CloseBtn>
+      </StoryDrawingModalContainer>
       {modalState === "wait" && <WaitToby />}
-      {modalState === "success" && (
-        <SuccessToby onClose={() => setModalState("none")} />
-      )}
-      {modalState === "fail" && (
-        <FailToby onClose={() => setModalState("none")} />
-      )}
-    </StoryDrawingModalContainer>
+      {modalState === "success" && <SuccessToby />}
+      {modalState === "fail" && <FailToby />}
+    </>
   );
 };
 
