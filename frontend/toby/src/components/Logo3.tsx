dee99247cb543215a1890bd2d9ebcdbf47a64f3c
오화석 <hwasuk528@naver.com>
2024-04-02 11:05:05 +0900

    :art: Feat: 드로잉

--- frontend/toby/src/components/Logo3.tsx
+++ frontend/toby/src/components/Logo3.tsx
@@ -32,16 +32,9 @@ const LogoImg = styled.img`
 
 const Logo = () => {
   const navigate = useNavigate();
-  const handleLogoClick = () => {
-    if (getUserStorage.accessToken) {
-      navigate("/main");
-    } else {
-      console.log("Access Denied. No accessToken found.");
-    }
-  };
 
   return (
-    <LogoWrapper onClick={handleLogoClick}>
+    <LogoWrapper onClick={() => navigate("/main")}>
       <LogoImg src="/Image/common/logoImage.png" alt="logo" />
     </LogoWrapper>
   );
