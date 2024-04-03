e19daec3231cd905ee5e2957b0ad63b9ede5dd4c
김태훈 <kth32105@naver.com>
2024-04-04 00:56:44 +0900

    :art: Feat: notfound 페이지, 클리어 페이지 수정

--- frontend/toby/src/pages/NotFound.tsx
+++ frontend/toby/src/pages/NotFound.tsx
@@ -7,9 +7,18 @@ const NotFoundContainer = styled.div`
   align-items: center;
   flex-direction: column;
   height: 100%;
+  position: relative;
+`;
+
+const Text = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  font-size: 2rem;
 `;
 
 const NotFoundImage = styled.img`
+  flex: 0 0 80%;
   width: auto;
   height: 100%;
 `;
@@ -17,7 +26,10 @@ const NotFoundImage = styled.img`
 const NotFound = () => {
   return (
     <NotFoundContainer>
-      <h1>404 Not Found</h1>
+      <Text>
+        404 Not Found <br />
+        없는 페이지 입니다.
+      </Text>
       <NotFoundImage src="/Image/toby/cuteRabbit.png" alt="notfound" />
     </NotFoundContainer>
   );
