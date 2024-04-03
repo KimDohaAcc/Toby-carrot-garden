2b1a0a986a44ccd1d2969bbe29e90f9a1150efbf
김태훈 <kth32105@naver.com>
2024-04-03 17:19:57 +0900

    :art: Feat: 수정

--- frontend/toby/src/config/apiConfig.tsx
+++ frontend/toby/src/config/apiConfig.tsx
@@ -34,6 +34,3 @@ export { swaggerapi };
 // base url: [`https://i10a202.p.ssafy.io`]
 
 // swagger url: [`http://localhost:8080/swagger-ui/index.html`]
-
-export const tempToken =
-  "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzE5OTcyODU2fQ.BRvOAWqb8P63P73BoMo1vokoWTgLmedEULuqlonphAeen2o81eO_08lYDeqRDFWCfk0V9iN7DWA89k7EZon8hg";
