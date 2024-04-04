4b0a4f598ae69949833e316312e4ff77dd4b74e5
오화석 <hwasuk528@naver.com>
2024-04-04 09:45:58 +0900

    드로잉 수정

--- frontend/toby/src/components/placeStory/StoryQuizDrawings.tsx
+++ frontend/toby/src/components/placeStory/StoryQuizDrawings.tsx
@@ -195,10 +195,11 @@ const StoryQuizDrawings = ({ imageUrl, quizId, content, index, place }) => {
     setIsPlaying(false);
   };
   const handleCanvasClick = () => {
-    if (!isSubmitted) {
-      // 제출 상태가 아닐 때만 모달을 열 수 있도록 조건 추가
-      openModal();
-    }
+    // if (!isSubmitted) {
+    //   // 제출 상태가 아닐 때만 모달을 열 수 있도록 조건 추가
+    //   openModal();
+    // }
+    openModal();
   };
   const openModal = () => {
     setIsModalOpen(true);
